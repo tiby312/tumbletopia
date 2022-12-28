0909eac085703c437638576999306f86e6b72cc0
Kenneth Reed <kenakioreed@gmail.com>
2022-12-27 19:49:37 -0500

    touch kind of working

--- src/lib.rs
+++ src/lib.rs
@@ -52,6 +52,16 @@ pub async fn main_entry() {
 
     let _handler = worker.register_event(&button, "click", |_| MEvent::ButtonClick);
 
+    let _handler = worker.register_event(&canvas, "touchstart", |_| MEvent::CanvasMouseDown);
+
+    let _handler = worker.register_event(&canvas, "touchend", |_| MEvent::CanvasMouseUp);
+
+    let _handler = worker.register_event(&canvas, "touchmove", |e|{
+        let [x, y] = convert_coord_touch(e.elem, e.event);
+        MEvent::CanvasMouseMove{x,y}
+    });
+
+
     let _handler = worker.register_event(&shutdown_button, "click", |_| MEvent::ShutdownClick);
 
     let _: () = response.next().await.unwrap_throw();
@@ -155,4 +165,47 @@ pub async fn worker_entry() {
 fn convert_coord(canvas: &web_sys::HtmlElement, event: &web_sys::Event) -> [f32; 2] {
     use wasm_bindgen::JsCast;
     shogo::simple2d::convert_coord(canvas, event.dyn_ref().unwrap_throw())
+}
+
+
+
+fn convert_coord_touch(canvas: &web_sys::HtmlElement, event: &web_sys::Event) -> [f32; 2] {
+    use wasm_bindgen::JsCast;
+    convert_coord_touch_inner(canvas, event.dyn_ref().unwrap_throw())[0]
+}
+
+
+///
+/// Convert a mouse event to a coordinate for simple2d.
+///
+pub fn convert_coord_touch_inner(canvas: &web_sys::HtmlElement, e: &web_sys::TouchEvent) -> Vec<[f32; 2]> {
+    let rect = canvas.get_bounding_client_rect();
+
+    let canvas_width: f64 = canvas
+        .get_attribute("width")
+        .unwrap_throw()
+        .parse()
+        .unwrap_throw();
+    let canvas_height: f64 = canvas
+        .get_attribute("height")
+        .unwrap_throw()
+        .parse()
+        .unwrap_throw();
+
+    let scalex = canvas_width / rect.width();
+    let scaley = canvas_height / rect.height();
+
+    let touches=e.touches();
+
+    let mut ans=vec!();
+    for a in 0..touches.length(){
+        let touch=touches.get(a).unwrap();
+        let x=touch.screen_x() as f64;
+        let y=touch.screen_y() as f64;
+
+        let [x, y] = [(x - rect.left()) * scalex, (y - rect.top()) * scaley];
+
+        ans.push([x as f32,y as f32]);
+    }
+    ans
 }
