060f83a1a3cd59bf441873c35e86ea893c7c2cb0
Kenneth Reed <kenakioreed@gmail.com>
2023-01-12 17:30:38 -0500

    newv

--- src/lib.rs
+++ src/lib.rs
@@ -659,7 +659,7 @@ fn projection(dim: [f32; 2]) -> impl matrix::MyMatrix + matrix::Inverse {
 fn view_projection(offset: [f32; 2], dim: [f32; 2]) -> impl matrix::MyMatrix + matrix::Inverse {
     use matrix::*;
 
-    projection(dim).chain(camera(offset, -800.0 + dim[1]).inverse())
+    projection(dim).chain(camera(offset, -600.0 + dim[1]*0.5).inverse())
 }
 
 const KEY_GLB: &'static [u8] = include_bytes!("../assets/key.glb");
