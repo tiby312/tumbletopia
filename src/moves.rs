ce16ba06c23c4996cc6c775cca5f8b768ffc2503
Kenneth Reed <kenakioreed@gmail.com>
2023-08-05 09:56:39 -0400

    newv

--- src/moves.rs
+++ src/moves.rs
@@ -30,21 +30,21 @@ mod inner_partial {
 
     //https://users.rust-lang.org/t/macro-to-dry-sync-and-async-code/67556
     macro_rules! resolve_inner_movement_impl {
-    ($args:expr, $($_await:tt)*) => {
-        {
-            let (start,path,doopa,game_view):(UnitData,movement::Path,_,&mut GameView<'_>)=$args;
-            let team=game_view.team;
-            let mut start = doopa
-                .wait_animation(ace::Movement::new(start,path), team)
-                $($_await)*;
+        ($args:expr, $($_await:tt)*) => {
+            {
+                let (start,path,doopa,game_view):(UnitData,movement::Path,_,&mut GameView<'_>)=$args;
+                let team=game_view.team;
+                let mut start = doopa
+                    .wait_animation(ace::Movement::new(start,path), team)
+                    $($_await)*;
 
-            start.position = path.get_end_coord(start.position);
+                start.position = path.get_end_coord(start.position);
 
-            let start:UnitData=start;
-            start
+                let start:UnitData=start;
+                start
+            }
         }
     }
-}
 
     use super::*;
     pub struct InnerPartialMove {
@@ -90,53 +90,53 @@ mod partial_move {
     use super::*;
 
     macro_rules! resolve_movement_impl {
-    ($args:expr,$namey:ident, $($_await:tt)*) => {
-        {
-            let (selected_unit,path, doopa,mut game_view):(GridCoord,movement::Path,_,&mut GameView<'_>)=$args;
+        ($args:expr,$namey:ident, $($_await:tt)*) => {
+            {
+                let (selected_unit,path, doopa,mut game_view):(GridCoord,movement::Path,_,&mut GameView<'_>)=$args;
 
-            let target_cell=path.get_end_coord(selected_unit);
+                let target_cell=path.get_end_coord(selected_unit);
 
-            let this_unit = game_view
-            .this_team
-            .find_take(&selected_unit)
-            .unwrap();
+                let this_unit = game_view
+                .this_team
+                .find_take(&selected_unit)
+                .unwrap();
 
-            let this_unit=InnerPartialMove::new(this_unit,path).$namey(&mut game_view,doopa)$($_await)*;
+                let this_unit=InnerPartialMove::new(this_unit,path).$namey(&mut game_view,doopa)$($_await)*;
 
-            game_view.this_team.add(this_unit);
+                game_view.this_team.add(this_unit);
 
-            let k=HandleSurround::new(target_cell).$namey(&mut game_view, doopa)$($_await)*;
+                let k=HandleSurround::new(target_cell).$namey(&mut game_view, doopa)$($_await)*;
 
 
-            //Need to add ourselves back so we can resolve and attacking groups
-            //only to remove ourselves again later.
-            let k = if let Some(k) = k {
-                let pp = k.position;
-                game_view.this_team.add(k);
+                //Need to add ourselves back so we can resolve and attacking groups
+                //only to remove ourselves again later.
+                let k = if let Some(k) = k {
+                    let pp = k.position;
+                    game_view.this_team.add(k);
 
-                for n in target_cell.to_cube().neighbours() {
-                    let _=HandleSurround::new(n.to_axial()).$namey(&mut game_view.not(), doopa)$($_await)*;
+                    for n in target_cell.to_cube().neighbours() {
+                        let _=HandleSurround::new(n.to_axial()).$namey(&mut game_view.not(), doopa)$($_await)*;
+                    }
+
+                    Some(game_view.this_team.find_take(&pp).unwrap())
+                } else {
+                    for n in target_cell.to_cube().neighbours() {
+                        let _=HandleSurround::new(n.to_axial()).$namey(&mut game_view.not(), doopa)$($_await)*;
+                    }
+
+                    None
+                };
+
+                if let Some(k) = k {
+                    game_view.this_team.add(k);
+                    ExtraMove::ExtraMove{pos:target_cell}
+                } else {
+                    //Finish this players turn.
+                    ExtraMove::FinishMoving
                 }
-
-                Some(game_view.this_team.find_take(&pp).unwrap())
-            } else {
-                for n in target_cell.to_cube().neighbours() {
-                    let _=HandleSurround::new(n.to_axial()).$namey(&mut game_view.not(), doopa)$($_await)*;
-                }
-
-                None
-            };
-
-            if let Some(k) = k {
-                game_view.this_team.add(k);
-                ExtraMove::ExtraMove{pos:target_cell}
-            } else {
-                //Finish this players turn.
-                ExtraMove::FinishMoving
             }
         }
     }
-}
 
     pub struct PartialMove {
         selected_unit: GridCoord,
@@ -189,34 +189,34 @@ mod invade {
     use super::*;
 
     macro_rules! resolve_invade_impl {
-    ($args:expr,$namey:ident, $($_await:tt)*) => {
-        {
-            let (selected_unit,target_coord,game_view,doopa):(GridCoord,GridCoord,&mut GameView<'_>,_)=$args;
-            let this_unit = game_view.this_team.find_take(&selected_unit).unwrap();
+        ($args:expr,$namey:ident, $($_await:tt)*) => {
+            {
+                let (selected_unit,target_coord,game_view,doopa):(GridCoord,GridCoord,&mut GameView<'_>,_)=$args;
+                let this_unit = game_view.this_team.find_take(&selected_unit).unwrap();
 
-            let _target = game_view.that_team.find_take(&target_coord).unwrap();
+                let _target = game_view.that_team.find_take(&target_coord).unwrap();
 
-            let path = movement::Path::new();
-            let m = this_unit.position.dir_to(&target_coord);
-            let path = path.add(m).unwrap();
+                let path = movement::Path::new();
+                let m = this_unit.position.dir_to(&target_coord);
+                let path = path.add(m).unwrap();
 
-            let mut this_unit=InnerPartialMove::new(this_unit,path).$namey(game_view,doopa)$($_await)*;
+                let mut this_unit=InnerPartialMove::new(this_unit,path).$namey(game_view,doopa)$($_await)*;
 
 
-            this_unit.position = target_coord;
+                this_unit.position = target_coord;
 
-            game_view.this_team.add(this_unit);
+                game_view.this_team.add(this_unit);
 
-            HandleSurround::new(target_coord).$namey(game_view,doopa)$($_await)*;
-            for n in target_coord.to_cube().neighbours() {
-                HandleSurround::new(n.to_axial()).$namey(&mut game_view.not(),doopa)$($_await)*;
+                HandleSurround::new(target_coord).$namey(game_view,doopa)$($_await)*;
+                for n in target_coord.to_cube().neighbours() {
+                    HandleSurround::new(n.to_axial()).$namey(&mut game_view.not(),doopa)$($_await)*;
+                }
+
+
+
             }
-
-
-
         }
     }
-}
 
     pub struct Invade {
         selected_unit: GridCoord,
@@ -264,45 +264,45 @@ pub use surround::HandleSurround;
 mod surround {
     use super::*;
     macro_rules! resolve_3_players_nearby_impl {
-    ($args:expr, $($_await:tt)*) => {{
-        let (n, doopa, game_view): (GridCoord, _, &mut GameView<'_>) = $args;
-        let team=game_view.team;
-        let n = n.to_cube();
-        if let Some(unit_pos) = game_view
-                    .this_team
-                    .warriors
-                    .find_ext_mut(&n.to_axial())
-        {
+        ($args:expr, $($_await:tt)*) => {{
+            let (n, doopa, game_view): (GridCoord, _, &mut GameView<'_>) = $args;
+            let team=game_view.team;
+            let n = n.to_cube();
+            if let Some(unit_pos) = game_view
+                        .this_team
+                        .warriors
+                        .find_ext_mut(&n.to_axial())
+            {
 
-        let unit_pos = unit_pos.0.position;
+            let unit_pos = unit_pos.0.position;
 
-        let nearby_enemies: Vec<_> = n
-            .neighbours()
-            .filter(|a| game_view.that_team.find_slow(&a.to_axial()).is_some())
-            .map(|a| a.to_axial())
-            .collect();
-        if nearby_enemies.len() >= 3 {
+            let nearby_enemies: Vec<_> = n
+                .neighbours()
+                .filter(|a| game_view.that_team.find_slow(&a.to_axial()).is_some())
+                .map(|a| a.to_axial())
+                .collect();
+            if nearby_enemies.len() >= 3 {
 
-            for n in nearby_enemies {
-                let unit_pos = game_view.this_team.find_take(&unit_pos).unwrap();
-                let them = game_view.that_team.find_take(&n).unwrap();
+                for n in nearby_enemies {
+                    let unit_pos = game_view.this_team.find_take(&unit_pos).unwrap();
+                    let them = game_view.that_team.find_take(&n).unwrap();
 
-                let [them, this_unit] = doopa
-                    .wait_animation(ace::Attack::new(them, unit_pos), team.not())
-                    $($_await)*;
+                    let [them, this_unit] = doopa
+                        .wait_animation(ace::Attack::new(them, unit_pos), team.not())
+                        $($_await)*;
 
-                game_view.that_team.add(them);
-                game_view.this_team.add(this_unit);
+                    game_view.that_team.add(them);
+                    game_view.this_team.add(this_unit);
+                }
+                Some(game_view.this_team.find_take(&unit_pos).unwrap())
+            } else {
+                None
             }
-            Some(game_view.this_team.find_take(&unit_pos).unwrap())
-        } else {
+        } else{
             None
         }
-    } else{
-        None
+        }};
     }
-    }};
-}
 
     pub struct HandleSurround {
         cell: GridCoord,
