356a01ad2108f605f0765d820988f3b1cd0b652b
tiby312 <kenakioreed@gmail.com>
2024-08-17 19:33:04 -0400

    n

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,13 @@ rm -r ../gridlock-web/pkg
 rm ../gridlock-web/index.html
 rm ../gridlock-web/gridlock_worker.js
 
+
+
 cp -r pkg ../gridlock-web
+cp favicon.ico ../gridlock-web
+cp manifest.json ../gridlock-web
 cp index.html ../gridlock-web
-cp game.html ../gridlock-web
+cp game1.html ../gridlock-web
 cp gridlock_worker.js ../gridlock-web
+cp gridlock_worker2.js ../gridlock-web
+
