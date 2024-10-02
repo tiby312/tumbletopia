8553a8783c1d04d99f986ecf16365f4be92db5f7
tiby312 <kenakioreed@gmail.com>
2024-10-02 09:04:14 -0400

    n

--- engine/src/unit.rs
+++ engine/src/unit.rs
@@ -417,7 +417,7 @@ impl Tribe {
         let val = bit0 | bit1 << 1 | bit2 << 2;
 
         if val == 7 {
-            return Some((2, ActiveTeam::Neutral));
+            return Some((1, ActiveTeam::Neutral));
         }
         if val == 0 {
             return Some((6, ActiveTeam::Neutral));
@@ -454,7 +454,7 @@ impl Tribe {
             ActiveTeam::White => self.team.inner.set(a, true),
             ActiveTeam::Black => self.team.inner.set(a, false),
             ActiveTeam::Neutral => {
-                let val = if stack == 2 {
+                let val = if stack == 1 {
                     7
                 } else if stack == 6 {
                     0
@@ -708,7 +708,7 @@ impl GameState {
         }
 
         for f in map.forests.iter_mesh(Axial::zero()) {
-            cells.add_cell(f, 2, ActiveTeam::Neutral);
+            cells.add_cell(f, 1, ActiveTeam::Neutral);
         }
 
         for m in map.mountains.iter_mesh(Axial::zero()) {
